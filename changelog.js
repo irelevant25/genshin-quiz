2396d01cea0dbecb21b5864d3ccbf1867bce95dc
Frantisek Pastorek <pastorek@alfabase.sk>
2025-02-22 23:47:32 +0100

    Create a changelog data. Adding version to the website. Creating modal with changelog data.

---
+++ changelog.js
@@ -0,0 +1,9 @@
+const changelogData = [
+    {
+        version: "0.1",
+        date: "2025-02-22",
+        changes: [
+            "Implemented a basic first version of the quiz website.",
+        ]
+    },
+];
