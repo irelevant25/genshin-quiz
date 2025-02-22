1dd6ebb4e18bd6be570f9b3ec9b0899d286047bf
Frantisek Pastorek <pastorek@alfabase.sk>
2025-02-23 00:09:09 +0100

    Beautified the UI of the change log.

--- changelog.js
+++ changelog.js
@@ -1,9 +1,24 @@
 const changelogData = [
     {
+        version: "0.2",
+        date: "",
+        changes: {
+            added: [],
+            fixed: [],
+            updated: [
+                "Beautified the UI of the change log.",
+            ],
+        },
+    },
+    {
         version: "0.1",
         date: "2025-02-22",
-        changes: [
-            "Implemented a basic first version of the quiz website.",
-        ]
-    },
+        changes: {
+            added: [
+                "Initial release of the quiz website.",
+            ],
+            fixed: [],
+            updated: [],
+        },
+    }
 ];
