4fa05eee1314ef65e68b210440e1c947972f132d
Frantisek Pastorek <pastorek@alfabase.sk>
2025-02-23 00:12:20 +0100

    Remove test data of change log.

--- changelog.js
+++ changelog.js
@@ -1,16 +1,5 @@
 const changelogData = [
     {
-        version: "0.2",
-        date: "",
-        changes: {
-            added: [],
-            fixed: [],
-            updated: [
-                "Beautified the UI of the change log.",
-            ],
-        },
-    },
-    {
         version: "0.1",
         date: "2025-02-22",
         changes: {
