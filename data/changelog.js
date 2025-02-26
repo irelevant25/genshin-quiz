0a8f26f801d50a1c99f14a66f7bd2193ca3f13f9
Frantisek Pastorek <pastorek@alfabase.sk>
2025-02-26 03:32:26 +0100

    Merge. Add new version to the changelog.

--- data/changelog.js
+++ data/changelog.js
@@ -1,5 +1,21 @@
 const changelog = [
     {
+        version: "0.2",
+        date: "2025-02-26",
+        changes: {
+            added: [
+                "Score display. Characters tries display.",
+                "Difficulties per try in banner quiz.",
+                "Character pixelate quiz.",
+                "Character mismatch quiz.",
+            ],
+            fixed: [],
+            updated: [
+                "Beautified the UI of the change log.",
+            ],
+        },
+    },
+    {
         version: "0.1",
         date: "2025-02-22",
         changes: {
