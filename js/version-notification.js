ffe2ea762595f55af0997d583be85854822cb018
Frantisek Pastorek <pastorek@alfabase.sk>
2025-03-05 02:38:45 +0100

    Remove unused code from version notification.

--- js/version-notification.js
+++ js/version-notification.js
@@ -8,44 +8,9 @@
     function renderVersionNotification(modalId = 'versionNotificationModal') {
         initializeChangelog([CHANGELOG[0]], 'versionNotificationContent');
         const modalElement = document.getElementById(modalId);
-        // if (!modalElement || !CHANGELOG || CHANGELOG.length === 0) return;
-
-        // // Get only the latest version
-        // const latestVersion = CHANGELOG[0];
-
-        // modalElement.querySelector('.version-tag').textContent = `v${latestVersion.version}`;
-        // modalElement.querySelector('.changelog-date').textContent = `v${latestVersion.date}`;
-
-        // let html = '';
-        // html += generateItemHtml(latestVersion.changes.added, 'added', 'plus-circle-fill');
-        // html += generateItemHtml(latestVersion.changes.fixed, 'fixed', 'bug-fill');
-        // html += generateItemHtml(latestVersion.changes.updated, 'updated', 'arrow-repeat');
-        // modalElement.querySelector('#versionNotificationItems').innerHTML = html;
 
         var myModal = new bootstrap.Modal(modalElement);
         myModal.show();
-    }
-
-    function generateItemHtml(items, type, icon) {
-        let html = '';
-        if (items.length > 0) {
-            html += `
-                <div class="change-heading ${type} animate-in delay">
-                    <i class="bi bi-${icon}"></i>
-                    <span class="capitalize">${type}</span>
-                </div>
-                <ul>
-            `;
-
-            items.forEach(item => {
-                html += `<li>${item}</li>`;
-            });
-
-            html += `
-                </ul>
-            `;
-        }
-        return html
     }
 
     window.renderVersionNotification = renderVersionNotification;
